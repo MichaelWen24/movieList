d5af6291ee457309d38d964be33314453bcd6d20
MichaelWen24 <wen820061663@gmail.com>
2021-01-08 14:47:30 -0500

    home page done

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const genresUrl = "https://api.themoviedb.org/3/genre/movie/list?api_key=f7b946d
 
 const model = {
     movies: [],
-    page: 0,
+    current_page: 1,
     total_pages: 0,
     total_results: 0,
     // genres: [],
@@ -26,9 +26,10 @@ const model = {
 
 // Controllers
 
-function fetchMovies(){
-    // fetch(`${baseUrl}${type}?api_key=f7b946d6ee4e6d5684eae0ca10ca98e4&page=${page}`)
-    return fetch(popularMoviesUrl)
+function fetchMovies(category, page){
+    category = (typeof category !== "undefined") ? category : "popular";
+    page = model.current_page;
+    return fetch(`${baseUrl}${category}?api_key=f7b946d6ee4e6d5684eae0ca10ca98e4&page=${page}`)
     .then((resp) => {
         if (resp.ok){
             return resp.json();
@@ -39,7 +40,7 @@ function fetchMovies(){
     })
     .then((data) => {
         model.movies = data.results;
-        model.page = data.page;
+        model.current_page = data.page;
         model.total_pages = data.total_pages;
         model.total_results = data.total_results;
         // console.log(model);
@@ -78,16 +79,34 @@ function fetchDetails(id) {
     });
 }
 
-// function getGenre(genre_id) {
-//     model.genres.find((genre) => {
-//         if(genre.id === genre_id){
-//             return genre.name;
-//         }
-//     });
-// }
+function getGenre(genre_id) {
+    model.genres.find((genre) => {
+        if(genre.id === genre_id){
+            return genre.name;
+        }
+    });
+}
 
-function fetchCompanies() {
-    return 
+function prevPage() {
+    let page = model.current_page;
+    if (page > 1) {
+        page --;
+        model.current_page = page
+    }
+    else {
+        console.log("There is no prev page");
+    }
+}
+
+function nextPage() {
+    let page = model.current_page;
+    if (page < model.total_pages) {
+        page ++;
+        model.current_page = page
+    }
+    else {
+        console.log("There is no next page");
+    }
 }
 
 // Views
@@ -97,39 +116,68 @@ function createMoviePage(movies) {
         const moviesPage = document.querySelector(".movies-page");
         moviesPage.innerHTML = "";
         movies.forEach((movie) => {
-            console.log(movie);
+            // if (movie !== null) {
             const movieContainer = createMovie(movie);
             moviesPage.appendChild(movieContainer);
+            // }
         });
     }
     else {
         console.log("Error while updating Movies page")
     }
+    // console.log(model.movies);
+}
+
+function updatePageCounter(value) {
+    const counter = document.querySelector(".page-counter");
+    counter.textContent = `${value} / ${model.total_pages}`;
 }
 
 function upadteMoviesPage() {
-    const promise = fetchMovies();
+    const counter = document.querySelector(".page-counter");
+    const category = getSelectorValue();
+    const page = model.current_page;
+    const promise = fetchMovies(category, page);
     promise.then(() => {
+        counter.textContent = `${model.current_page} / ${model.total_pages}`;
        createMoviePage(model.movies);
     });
-}
-
-function updateView() {
-    upadteMoviesPage();
 }
 
 function createMovie(movie) {
     const movieDiv = document.createElement("div");
     movieDiv.className = "movie-container";
     movieDiv.id = movie.id;
-    const innerHtml = `
-    <img class="movie-img" src="${imageUrl}${movie.poster_path}"/>
-    <h3 class="movie-name">${movie.title}</h3>
-    <div class="movie-icon">
-        <ion-icon name="star"></ion-icon>
-        <label class="vote">${movie.vote_average}</label>
-        <ion-icon name="heart-empty"></ion-icon>
-    </div>`;
+    let innerHtml = "";
+    if (movie.poster_path !== null) {
+        innerHtml = `
+        <img class="movie-img" src="${imageUrl}${movie.poster_path}"/>
+        <h3 class="movie-name">${movie.title}</h3>
+        <div class="movie-icon">
+            <div class="star-vote">
+                <ion-icon name="star"></ion-icon>
+                <label class="vote">${movie.vote_average}</label>
+            </div>
+            <div class="heart">
+                <ion-icon name="heart-empty"></ion-icon>
+            </div>
+        </div>`;
+    }
+    else{
+        innerHtml = `
+        <img class="movie-img" src="https://t4.ftcdn.net/jpg/02/07/87/79/360_F_207877921_BtG6ZKAVvtLyc5GWpBNEIlIxsffTtWkv.jpg"/>
+        <h3 class="movie-name">${movie.title}</h3>
+        <div class="movie-icon">
+            <div class="star-vote">
+                <ion-icon name="star"></ion-icon>
+                <label class="vote">${movie.vote_average}</label>
+            </div>
+            <div class="heart">
+                <ion-icon name="heart-empty"></ion-icon>
+            </div>
+        </div>`;
+    }
+
     movieDiv.innerHTML = innerHtml;
     return movieDiv;
 }
@@ -139,7 +187,7 @@ function fillDetails(id) {
     const detailsPromise = fetchDetails(id);
     detailsPromise
     .then((movie) => {
-        console.log(movie);
+        // console.log(movie);
         detailsPage.id = id;
 
         const imageDiv = document.createElement("div");
@@ -186,7 +234,6 @@ function fillDetails(id) {
         companies.className = "companies";
         companies.textContent = "Production Companies";
     
-        // const companiesArray = fetchCompanies(id);
         const companyContainer = document.createElement("div");
         companyContainer.className = "companies-img";
         companyContainer.innerHTML = ""
@@ -210,14 +257,112 @@ function fillDetails(id) {
 
         detailsPage.appendChild(imageDiv);
         detailsPage.appendChild(detailsContainer);
-        
     });   
 }
 
+function getSelectorValue() {
+    const movieCategorySelect = document.querySelector(".movie-selector");
+    const index = movieCategorySelect.selectedIndex;
+    const category = movieCategorySelect[index].value;
+    return category;
+}
+
+function getMovieId(e) {
+    const target = e.target.closest(".movie-container");
+    console.log(target);
+    if (target.classList.contains("movie-img") || target.classList.contains("movie-name")) {
+        console.log(target);
+        const movie = target.parentNode;
+        console.log(movie);
+    }
+}
+
+function updatePage(e) {
+    const target = e.target;
+    if (target.classList.contains("prev")) {
+        prevPage();
+        updateView();
+    } 
+    if (target.classList.contains("next")) {
+        nextPage();
+        updateView();
+    }
+}
+
+// function getPageValue() {
+//     const target = e.target;
+//     let value  = 1;
+//     const prev = document.querySelector(".prev");
+//     const next = document.querySelector(".next");
+//     const counter = document.querySelector(".page-counter");
+
+//     if (target.classList.conatins("prev")) {
+//         if (value > 1) {
+//             value --;
+//             updatePageCounter(value);
+//             console.log(value);
+//         }
+//         else {
+//             console.log("There is no prev page");
+//         }
+//     }
+
+//     if (target.classList.conatins("next")) {
+//         if (value < model.total_pages) {
+//             value ++;
+//             updatePageCounter(value);
+//             console.log(value);
+//         }
+//         else {
+//             console.log("There is no next page");
+//         }
+//     }
+
+    // prev.addEventListener("click", () =>{
+    //     if (value > 1) {
+    //         value --;
+    //         updatePageCounter(value);
+    //         console.log(value);
+    //     }
+    //     else {
+    //         console.log("There is no prev page");
+    //     }
+    // });
+
+    // next.addEventListener("click", () => {
+    //     if (value < model.total_pages) {
+    //         value ++;
+    //         updatePageCounter(value);
+    //         console.log(value);
+    //     }
+    //     else {
+    //         console.log("There is no next page");
+    //     }
+    // });
+
+//     return value;
+// }
+
+function updateView() {
+    upadteMoviesPage();
+}
 
 const loadEvent = () => {
-    updateView();
-    fillDetails(464052);
+    if(model.movies.length === 0){
+        updateView();
+    }
+
+    const movieCategory = document.querySelector(".movie-selector");
+    const page = document.querySelector(".page-label");
+    const movieDiv = document.querySelector(".movies-page");
+    
+    movieCategory.addEventListener("change", () => {
+        model.current_page = 1;
+        updateView();
+    });
+    page.addEventListener("click", updatePage);
+    movieDiv.addEventListener("click", getMovieId);
+    
 }
 
 loadEvent();
